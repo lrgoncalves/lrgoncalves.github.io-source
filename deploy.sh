0ea9766005c7b6c07bbf9d2718df865b70c6d4af
Leandro Gonçalves <leandro.rocha.goncalves@gmail.com>
2016-02-16 08:38:37 -0200

    add comments

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,19 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS" == "true" ]; then
         echo -e "Logging with travis user\n"
         git config --global user.email "leandro.rocha.goncalves@gmail.com"
-        git config --global user.name "Travis"
+        git config --global user.name "Leandro Gonçalves"
     fi
     #using token clone gh-pages branch
+    echo -e "git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null"
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+
     #go into directory and copy data we're interested in to that directory
     cd built_website
+    echo -e "rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* ."
     rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
     #add, commit and push files
     git add -f .
+    echo -e "git commit -m Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
     git push -fq origin $BRANCH > /dev/null
     echo -e "Deploy completed\n"
