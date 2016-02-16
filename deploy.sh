ca284d1b2498aec5cc68072642e16ed00817b24c
Leandro Gonçalves <leandro.rocha.goncalves@gmail.com>
2016-02-16 10:19:12 -0200

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,20 @@
-#!/usr/bin/env bash
-BRANCH=master
-TARGET_REPO=lrgoncalves/lrgoncalves.github.io
-PELICAN_OUTPUT_FOLDER=output
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
 
-echo -e "Testing travis-encrypt"
-echo -e "$VARNAME"
+rm -rf public
+mkdir public
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo -e "Starting to deploy to Github Pages\n"
-    if [ "$TRAVIS" == "true" ]; then
-        echo -e "Logging with travis user\n"
-        git config --global user.email "leandro.rocha.goncalves@gmail.com"
-        git config --global user.name "Leandro Gonçalves"
-    fi
-    #using token clone gh-pages branch
-    echo -e "git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null"
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+# config
+git config --global user.email "leandro.rocha.goncalves@gmail.com"
+git config --global user.name "Leandro Gonçalves"
 
-    #go into directory and copy data we're interested in to that directory
-    cd built_website
-    echo -e "rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* ."
-    rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
-    #add, commit and push files
-    git add -f .
-    echo -e "git commit -m Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-    echo -e "git push -fq origin $BRANCH > /dev/null"
-    git push -fq origin $BRANCH > /dev/null
-    echo -e "Deploy completed\n"
-fi
+# build (CHANGE THIS)
+make
+
+# deploy
+cd public
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
